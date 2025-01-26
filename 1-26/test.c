d3dbbcf95808c71583e2d8a1078bcdde5092a0e0
佳斌 李 <l3234216896@gmail.com>
2025-01-26 15:21:11 +0800

    学到嵌套if

---
+++ 1-26/test.c
@@ -0,0 +1,82 @@
+﻿#include<stdio.h>
+
+int main()
+{
+	//int num = 0;
+	//scanf_s("%d", &num);
+	//if (num % 2 == 1) 
+	//	printf("%d为奇数", num);
+	//else
+	//
+	//	printf("%d为偶数", num);
+	
+
+	//int age = 0;
+	//scanf_s("%d", &age);
+	//if (age >= 18)
+	//	printf("成年\n");
+	//else
+
+	//	printf("未成年");
+	
+
+/*	int age = 0;
+	scanf_s("%d", &age);
+	if (age >= 18)
+		printf("成年\n");
+		printf("asdfghjkl\n");   独立存在，不管if真假，asdfghjkl都存在   （if只能控制一条语句） */ 
+
+
+	/*int age = 0;
+	scanf_s("%d", &age);
+	if (age >= 18)
+	{
+		printf("成年\n");
+		printf("asdfghjkl\n");
+	}*/
+	
+	/*int num = 0;
+	printf("输入数字");
+	scanf_s("%d", &num);
+	if (num == 0)
+		printf("为0\n");
+	else if (num > 0)
+		printf("正数");
+	else
+		printf("负数");*/
+
+
+		//int num = 0;
+		//scanf_s("%d", &num);
+		//if (num == 0)
+		//	printf("输⼊的数字是0\n");
+		//else if (num > 0) //这⾥的if 相当于嵌套在else语句中，形成了嵌套结构
+		//	printf("输⼊的数字是正数\n");
+		//else
+		//	printf("输⼊的数字是负数\n");
+	
+
+
+	//int num = 0;
+	//printf("请输入一个数");
+	//scanf_s("%d", &num);
+	//if (num > 0)
+	//{
+	//	if (num % 2 == 1)
+	//		printf("%d是奇数", num);
+
+	//	else
+	//	{
+	//		printf("%d是偶数", num);
+	//	}
+	//}
+
+	//else
+	//	printf("非正数");
+
+
+
+	return 0;
+
+
+}
